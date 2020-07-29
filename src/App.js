6787d2152ae0d575378ac9317441687eb02bce8e
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-29 15:21:35 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+/* 
+React.createELement(
+  what kind of tag it is,
+  attributes we want to give to component,
+  children
+);
+*/
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+// rendering on the DOM
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

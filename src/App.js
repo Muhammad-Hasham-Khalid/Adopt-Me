38f230db38bfead262640d8a383d543b5a172956
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-30 11:53:26 +0500

    Add Reach Router

--- src/App.js
+++ src/App.js
@@ -8,14 +8,21 @@ React.createElement(
 
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );

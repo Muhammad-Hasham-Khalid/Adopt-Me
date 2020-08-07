44bb55009f5c388c995900817c881a48fa766a12
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-08-07 21:20:51 +0500

    Added emotion

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ React.createElement(
 
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import DetailsWthErrorBoundary from "./Details";
 import ThemeContext from "./ThemeContext";
+import Navbar from "./Navbar";
 
 const App = () => {
   const themeHook = useState("peru");
@@ -20,9 +21,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
+          <Navbar />
           <Router>
             <SearchParams path="/" />
             <DetailsWthErrorBoundary path="/details/:id" />
---
+++ src/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Navbar = () => {
+  const [padding] = useState(15);
+
+  return (
+    <header
+      css={css`
+        background-color: ${colors.secondary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+          cursor: pointer;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
+          &:hover {
+            animation: 1s ${spin} linear infinite reverse;
+            text-decoration: underline;
+          }
+        `}
+        role="img"
+        aria-label="logo"
+      >
+        🐩
+      </span>
+    </header>
+  );
+};
+
+export default Navbar;

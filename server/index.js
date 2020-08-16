7d47d4ddfc9273c104cea97ca2f049c79b82f894
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-08-16 16:45:56 +0500

    renderToNodeStream

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import React from "react";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { ServerLocation } from "@reach/router";
 import fs from "fs";
 import App from "../src/App";
@@ -15,15 +15,21 @@ const app = express();
 
 app.use("/dist", express.static("dist"));
 app.use((req, res) => {
+  res.write(parts[0]);
   const reactMarkup = (
     <ServerLocation url={req.url}>
       <App />
     </ServerLocation>
   );
 
-  res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
+  const stream = renderToNodeStream(reactMarkup);
 
-  res.end();
+  stream.pipe(res, { end: false });
+
+  stream.on("end", () => {
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 console.log("Listening on " + PORT);
